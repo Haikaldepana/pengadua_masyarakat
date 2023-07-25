aa08a10ba60af1e2e3b03b1988199381b6dc19fa
Haikaldepana <111264767+Haikaldepana@users.noreply.github.com>
2023-07-25 11:49:20 +0700

    Add files via upload

---
+++ rumah.php
@@ -0,0 +1,11 @@
+<?php
+  class rumah{
+    public $warna = "merah";
+    public $pintu = 2;
+    public $jendela = 4;
+    public $genteng = 20;
+    public $lantai = "marmer";
+    public $pagar = "besi";
+  }
+  $rumah1 = new rumah();
+  echo $rumah1->warna;
